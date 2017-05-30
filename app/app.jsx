d9630edf748f9bd862df0c6ac9513901de1f4ac0
Rakesh <rakeshpetit@gmail.com>
2017-05-30 19:45:19 +0530

    First Commit

---
+++ app/app.jsx
@@ -0,0 +1,19 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var Main = require('Main');
+var Countdown = require('Countdown');
+
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
+
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+      <Route path="Countdown" component={Countdown}/>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+);
---
+++ app/components/Countdown.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+var Countdown = (props) => {
+  return(
+    <div>
+      <h1 className="text-center page-title">Countdown</h1>
+      <p className="text-center page-title">Countdown timer</p>
+    </div>
+
+  )
+}
+
+module.exports = Countdown;
